c7e4b53e64f3170dd815993be239e1bac5dd603c
Oussama Aouina <oussamaaouina99@gmail.com>
2024-02-15 08:57:45 +0100

    adding the aspect class for the aop

---
+++ src/main/java/com/aop/command/aspect/Aspect.java
@@ -0,0 +1,32 @@
+package com.aop.command.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@org.aspectj.lang.annotation.Aspect
+@Slf4j
+@Component
+public class Aspect {
+    @Pointcut("execution(* com.aop.command.controller.*.*(..))")
+    public void loggingPointCut(){
+    }
+
+    @Before("loggingPointCut()")
+    public void before( JoinPoint joinPoint ){
+        log.info("Before method invoked::"+joinPoint.getSignature());
+    }
+    @After("loggingPointCut()")
+    public void after(JoinPoint joinPoint){
+        log.info("transction submitted::"+joinPoint.getKind());
+    }
+
+    @AfterThrowing("loggingPointCut()")
+    public void afterthrow(JoinPoint joinPoint){
+        log.info("transction echoued::"+joinPoint.getTarget());
+    }
+}
