3f3719b5fe987baf8a54c088687778686fe17352
Oussama Aouina <oussamaaouina99@gmail.com>
2024-02-19 23:02:04 +0100

    creating stock topic with kafka

--- src/main/java/com/aop/command/config/KafkaTopicConfig.java
+++ src/main/java/com/aop/command/config/KafkaTopicConfig.java
@@ -1,2 +1,16 @@
-package com.aop.command.config;public class KafkaTopicConfig {
+package com.aop.command.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Bean
+    public NewTopic stockTopic(){
+        return TopicBuilder.
+                name("stock").
+                build();
+    }
 }
