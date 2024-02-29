f413eaacb38cd9e83cfd9c1095ba916f913f3b97
Oussama Aouina <oussamaaouina99@gmail.com>
2024-03-01 00:26:23 +0100

    update the controller and the service to handle the UPDATE and the DELETE

--- src/main/java/com/aop/command/controller/ProductController.java
+++ src/main/java/com/aop/command/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.aop.command.model.Product;
 import com.aop.command.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +19,17 @@ public class ProductController {
     @PostMapping("/save")
     public void saveProduct(@RequestBody Product product) {
         productService.addProduct(product);
+    }
 
+    @PutMapping("update/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable String id,@RequestBody Product product){
+        productService.updateProduct(id,product);
+        return ResponseEntity.ok("product :"+id+"updated");
+    }
+
+    @DeleteMapping("delete")
+    public ResponseEntity<String> updateProduct(@PathVariable String id){
+        productService.deleteProduct(id);
+        return ResponseEntity.ok("product :"+id+"deleted");
     }
 }
--- src/main/java/com/aop/command/service/ProductService.java
+++ src/main/java/com/aop/command/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.aop.command.service;
 
+import com.aop.command.dto.EventTypes;
+import com.aop.command.dto.KafkaEvent;
 import com.aop.command.model.Product;
 import com.aop.command.repository.ProductRepo;
 import lombok.RequiredArgsConstructor;
@@ -17,9 +19,34 @@ public class ProductService {
     @Autowired
     private KafkaJsonService kafkaJsonService;
 
-    public void addProduct(Product product) {
-        productRepo.save(product);
-        //kafkaService.sendMessage("product added:"+product.getId());
-        kafkaJsonService.sendProduct(product);
+    @Autowired
+    private KafkaService kafkaService;
+
+//    public void addProduct(Product product) {
+//        productRepo.save(product);
+//        kafkaJsonService.sendProduct(product);
+//    }
+public void addProduct(Product product) {
+    productRepo.save(product);
+    KafkaEvent kafkaEvent = new KafkaEvent(EventTypes.addProduct,product);
+    kafkaJsonService.sendMessage(kafkaEvent);
+}
+
+    public void updateProduct(String id, Product product) {
+        Product old = productRepo.findById(id).get();
+        old.setName(product.getName());
+        old.setDescription(product.getDescription());
+        old.setPrice(product.getPrice());
+        productRepo.save(old);
+        KafkaEvent updateEvnent = new KafkaEvent(EventTypes.updateProduct,old);
+        kafkaJsonService.sendMessage(updateEvnent);
+    }
+
+    public void deleteProduct(String id) {
+        Product old = productRepo.findById(id).get();
+        productRepo.delete(old);
+        KafkaEvent deleteEvent = new KafkaEvent(EventTypes.deleteProduct,old);
+        kafkaJsonService.sendMessage(deleteEvent);
+
     }
 }
